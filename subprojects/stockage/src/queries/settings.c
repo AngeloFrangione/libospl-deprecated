2fbd6f1167268f2dc8f6b03f48bc0a661e5aa4ac
Angelo Frangione <angelo.frangione@gmail.com>
2020-05-21 12:30:08 +0200

    [STKG] finalizes setting wrapper

--- subprojects/stockage/src/queries/settings.c
+++ subprojects/stockage/src/queries/settings.c
@@ -9,30 +9,56 @@ static int callback(t_db *param, int argc, char **argv, char **column)
 	return 0;
 }
 
-int insert_setting(char *name, int value)
+int insert_setting(char *dbpath, char *name, char *value)
 {
 	t_db	db;
 	char	query[BUFFER_SIZE] = "insert into settings (name, value) ";
+	sprintf(query + 35, "values (\"%s\", \"%s\");", name, value);
 
 	init_connection(&db);
-	init_db_transaction_rw(&db, "test.db");
-	sprintf(query + 35, "values (\"%s\", %d);", name, value);
+	init_db_transaction_rw(&db, dbpath);
 	insert_transaction(query, &db);
 	finalize_transaction(&db);
 	return 0;
 }
 
-int select_setting(char *name, int *value)
+int update_setting(char *dbpath, char *name, char *value)
+{
+	t_db	db;
+	char	query[BUFFER_SIZE] = "update settings set value=";
+
+	sprintf(query + 26, "\"%s\" where name=\"%s\";", value, name);
+	init_connection(&db);
+	init_db_transaction_rw(&db, dbpath);
+	insert_transaction(query, &db);
+	finalize_transaction(&db);
+	return 0;
+}
+
+int select_setting(char *dbpath, char *name, char *value)
 {
 	t_db	db;
 	char	query[BUFFER_SIZE] = "select * from settings ";
-
-	init_connection(&db);
-	init_db_transaction_rw(&db, "test.db");
+	
 	sprintf(query + 23, "where name = \"%s\"", name);
+	init_connection(&db);
+	init_db_transaction_rw(&db, dbpath);	
 	select_transaction_data(query, &db, callback, &db);
-	*value = atoi((char*)db.param);
+	strcpy(value, db.param);
 	free(db.param);
+	finalize_transaction(&db);
+	return 0;
+}
+
+int delete_setting(char *dbpath, char *name)
+{
+	t_db	db;
+	char	query[BUFFER_SIZE] = "delete from settings where name=";
+	
+	sprintf(query + 32, "%s;", name);
+	init_connection(&db);
+	init_db_transaction_rw(&db, dbpath);
+	insert_transaction(query, &db);
 	finalize_transaction(&db);
 	return 0;
 }
