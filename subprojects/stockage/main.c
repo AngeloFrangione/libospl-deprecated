af4b82db666816696524897577024e98619be6a3
Angelo Frangione <angelo.frangione@gmail.com>
2020-05-21 12:30:08 +0200

    [STKG][ADD] finalizes setting wrapper

--- subprojects/stockage/main.c
+++ subprojects/stockage/main.c
@@ -3,11 +3,16 @@
 
 int main(int argc, char const *argv[])
 {
-	int value;
+	char value[10] = {0};
 
 
 	create_database("test.db");
-	select_setting("version", &value);
-	printf("%d\n", value);
+	delete_setting("test.db", "name");
+	insert_setting("test.db", "version", "0.1.0");
+	select_setting("test.db", "version", value);
+	printf("%s\n", value);
+	update_setting("test.db", "version", "0.52");
+	select_setting("test.db", "version", value);
+	printf("%s\n", value);
 	return 0;
 }
