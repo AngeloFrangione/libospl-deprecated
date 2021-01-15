c627725096fb5cf31598a7e0a5c9b2b94cbb5c02
Angelo Frangione <angelo.frangione@gmail.com>
2021-01-15 01:51:16 -0500

    [OSPL] return error if photo does not exists

--- test_files/test_22_fail_del_photo.c
+++ test_files/test_22_fail_del_photo.c
@@ -9,14 +9,33 @@ int main(void)
 {
 	struct timeval nstime;
 	char lib_name[50];
+	char tmp[4096];
+	int ret;
 
 	gettimeofday(&nstime, NULL);
 	srand(nstime.tv_usec);
 	int r = rand();
 	sprintf(lib_name, "test_Library_22_%d", r);
-	if (ospl_photo_delete(lib_name, 1) >= 0)
+	if ((ret = ospl_photo_delete(lib_name, 1)) != ERR_DB)
 	{
-		printf("no error executing ospl_photo_delete\n");
+		printf("wrong error returned executing ospl_photo_delete(expected: %d): %d\n",ERR_DB, ret);
+		return 0;
+	}
+	if (ospl_create_library(lib_name) < 0)
+	{
+		printf("Error executing ospl_create_library\n");
+		printf("lib_name: %s\n", lib_name);
+		return 0;
+	}
+	if ((ret = ospl_photo_delete(lib_name, 1)) != ERR_PHO_NF)
+	{
+		printf("wrong error returned executing ospl_photo_delete(expected: %d): %d\n",ERR_PHO_NF, ret);
+		return 0;
+	}
+	sprintf(tmp, "rm -rf %s", lib_name);
+	if (system(tmp))
+	{
+		printf("executing the system command failed\n");
 		return 0;
 	}
 	return 1;
