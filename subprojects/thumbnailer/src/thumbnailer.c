8fd9cf1e2a24dfb65d2e4d91829a2d9860ccd50e
Angelo Frangione <afrangio@student.42.fr>
2021-03-23 22:22:42 +0100

    [thumbnailer] alternative to magic for windows

--- subprojects/thumbnailer/src/thumbnailer.c
+++ subprojects/thumbnailer/src/thumbnailer.c
@@ -18,32 +18,56 @@
 	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include <magic.h>
+#ifndef _WIN32
+# include <magic.h>
+#endif
 #include "epeg.h"
 #include "thumbnailer.h"
 
-static int get_magic(char *actual_file, char **magic)
+
+#ifndef _WIN32
+/**
+  * \brief Get the mime type from file with the magic number
+  *
+  * \param file_path path of the file to look for the magic
+  * \param magic buffer were the mimetype will be put
+  * in (don't have to be allocaded)
+  * \return 1 if an error occured
+  * \return 0 if the mime type was succefully put into magic
+  */
+int get_magic(char *file_path, char **magic)
 {
 	magic_t magic_cookie;
 
 	magic_cookie = magic_open(MAGIC_MIME_TYPE);
-	if (magic_cookie == NULL)
-		return 1;
-	if (magic_load(magic_cookie, NULL) != 0)
+	if (!magic_cookie)
 	{
+		printf("unable to initialize magic library\n");
+		return 1;
+	}
+	if (magic_load(magic_cookie, NULL))
+	{
+		printf("cannot load magic database - %s\n", magic_error(magic_cookie));
 		magic_close(magic_cookie);
 		return 1;
 	}
-	*magic = strdup(magic_file(magic_cookie, actual_file));
+	*magic = strdup(magic_file(magic_cookie, file_path));
 	magic_close(magic_cookie);
 	return 0;
 }
 
-static int is_supported(char *src)
+/**
+  * \brief Check if an image is supported
+  *
+  * \param src path of the image to check
+  * \return the index of the MimeType + 1 if supported
+  * \return 0 if not supported
+  */
+int is_supported(char *src)
 {
 	char **p = SUPPORTED_IMAGES;
 	char *magic = NULL;
-	
+
 	if (get_magic(src, &magic))
 		return 0;
 	for (int i = 0; i < NB_SUPPORTED_IMAGES; i++)
@@ -51,12 +75,55 @@ static int is_supported(char *src)
 		if (!strcmp(p[i], magic))
 		{
 			free(magic);
-			return i;
+			return i + 1;
 		}
 	}
 	free(magic);
 	return 0;
 }
+#endif
+
+#ifdef _WIN32
+int get_ext(char *src, char **ext)
+{
+	int i = 0, len;
+
+	len = strlen(src);
+	while (src[len - i] != '.' || i == len)
+		++i;
+	*ext = calloc(len - i, sizeof(char *));
+	strcpy(*ext, &src[len - i]);
+	return 0;
+}
+
+/**
+  * \brief Check if an image is supported
+  *
+  * \param src path of the image to check
+  * \return the index of the MimeType + 1 if supported
+  * \return 0 if not supported
+  */
+int is_supported(char *src)
+{
+	char **p = SUPPORTED_IMAGE_EXT;
+	char *ext = NULL;
+
+	if (get_ext(src, &ext))
+		return 0;
+	for (int i = 0; i < NB_SUPPORTED_EXT; i++)
+	{
+		if (!strcmp(p[i], ext))
+		{
+			free(ext);
+			return i + 1;
+		}
+	}
+	free(ext);
+	return 0;
+}
+#endif
+
+
 
 int create_thumbnail_jpeg(char *src, char *dst, int width)
 {
@@ -82,7 +149,7 @@ int create_thumbnail_jpeg(char *src, char *dst, int width)
 		if (r) 
 			return E_ENCODE;
 		else
-			return 0;
+			return 1;
 }
 
 int create_thumbnail(char *src, char *dst, int width)
@@ -92,6 +159,11 @@ int create_thumbnail(char *src, char *dst, int width)
 		case JPEG:
 			return create_thumbnail_jpeg(src, dst, width);
 			break;
+		#ifdef _WIN32
+		case JPG:
+			return create_thumbnail_jpeg(src, dst, width);
+			break;
+		#endif
 		default:
 			return E_NOSUPPORT;
 			break;
